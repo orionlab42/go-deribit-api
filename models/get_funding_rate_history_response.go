a17bea4ed6edcbb56e03c993c476c49e4d1610a8
annakallo <anna.kallo@gmail.com>
2021-02-18 12:03:18 +0100

    Correction on Get funding rate history response

--- models/get_funding_rate_history_response.go
+++ models/get_funding_rate_history_response.go
@@ -1,9 +1,11 @@
 package models
 
-type GetFundingRateHistoryResponse struct {
+type GetFundingRateHistoryData struct {
 	Timestamp      int64   `json:"timestamp"`
 	IndexPrice     float64 `json:"index_price"`
 	PrevIndexPrice float64 `json:"prev_index_price"`
 	Interest1H     float64 `json:"interest_1h"`
 	Interest8H     float64 `json:"interest_8h"`
 }
+
+type GetFundingRateHistoryResponse []GetFundingRateHistoryData
