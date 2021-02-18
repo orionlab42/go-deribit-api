edefab5766e69c2429a7afb72155812e36cc804b
annakallo <anna.kallo@gmail.com>
2021-02-18 11:32:45 +0100

    Correction on Get funding chart data response

--- models/get_funding_chart_data_response.go
+++ models/get_funding_chart_data_response.go
@@ -1,9 +1,13 @@
 package models
 
+type GetFundingChartData struct {
+	IndexPrice float64 `json:"index_price"`
+	Interest8H float64 `json:"interest_8h"`
+	Timestamp  int64   `json:"timestamp"`
+}
+
 type GetFundingChartDataResponse struct {
-	CurrentInterest float64     `json:"current_interest"`
-	Data            [][]float64 `json:"data"`
-	IndexPrice      float64     `json:"index_price"`
-	Interest8H      float64     `json:"interest_8h"`
-	Max             float64     `json:"max"`
+	CurrentInterest float64               `json:"current_interest"`
+	Data            []GetFundingChartData `json:"data"`
+	Interest8H      float64               `json:"interest_8h"`
 }
